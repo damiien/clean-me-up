00ab27a3d576e6149a3129e3b74fbd987f23fadc
dammiien <dame.insct@gmail.com>
2021-08-20 10:41:44 +0200

    Sonar Analyze

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD $DOCKER_REGISTRY
-docker pull docker.pkg.github.com/newsroom-technologies/arktype/clean-me-up-rest-api
+docker pull docker.pkg.github.com/damiien/clean-me-up-rest-api/clean-me-up-rest-api
 docker stop rest-api || true && docker rm rest-api || true
-docker run --network host -e SPRING_PROFILES_ACTIVE=default -d --name rest-api docker.pkg.github.com/arktype/clean-me-up-rest-api
+docker run --network host -e SPRING_PROFILES_ACTIVE=dev -d --name rest-api docker.pkg.github.com/damiien/clean-me-up-rest-api/clean-me-up-rest-api
