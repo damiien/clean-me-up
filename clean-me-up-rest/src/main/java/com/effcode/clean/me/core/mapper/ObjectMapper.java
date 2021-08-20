d4216f8c9363183a46934813009b90ba23f40c5f
dammiien <dame.insct@gmail.com>
2021-08-20 11:07:36 +0200

    Sonar Analyze

--- clean-me-up-rest/src/main/java/com/effcode/clean/me/core/mapper/ObjectMapper.java
+++ clean-me-up-rest/src/main/java/com/effcode/clean/me/core/mapper/ObjectMapper.java
@@ -15,15 +15,16 @@ import java.text.SimpleDateFormat;
 /**
  * Object mapper decorator component, provides singleton instance with application specific configuration
  *
+ * @author dame.gjorgjievski
+ * @version 1.0
  * @see com.fasterxml.jackson.databind.ObjectMapper
  * @see Component
  */
 @Component
 public class ObjectMapper extends com.fasterxml.jackson.databind.ObjectMapper {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ObjectMapper.class);
     public static final String DATE_FORMAT_PATTERN = "yyyy-MM-dd hh:mm:ss";
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_PATTERN);
+    private static final Logger LOG = LoggerFactory.getLogger(ObjectMapper.class);
     private static ObjectMapper instance;
 
     public ObjectMapper() {
@@ -32,7 +33,7 @@ public class ObjectMapper extends com.fasterxml.jackson.databind.ObjectMapper {
         registerModule(new JavaTimeModule());
         disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         enable(JsonGenerator.Feature.IGNORE_UNKNOWN);
-        setDateFormat(DATE_FORMAT);
+        setDateFormat(new SimpleDateFormat(DATE_FORMAT_PATTERN));
         setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
         disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         configure(SerializationFeature.CLOSE_CLOSEABLE, true);
@@ -71,10 +72,11 @@ public class ObjectMapper extends com.fasterxml.jackson.databind.ObjectMapper {
 
     /**
      * Deserializes a specified JSON formatted content string to an object of required type. Internally ignores thrown instances of
-     *      * {@link JsonProcessingException} and logs the stacktrace, in which case {@code null} is returned.
+     * * {@link JsonProcessingException} and logs the stacktrace, in which case {@code null} is returned.
+     *
      * @param content JSON formatted content string
-     * @param clazz output object class
-     * @param <T> output object type
+     * @param clazz   output object class
+     * @param <T>     output object type
      * @return deserialized object instance, or {@code null} if deserialization fails.
      */
     public final <T> T toObject(final String content, final Class<T> clazz) {
